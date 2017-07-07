8b8e12084ca764c28ff73d2664eefd69bb9423ad
rayGreycloud <rayGreycloud@gmail.com>
2017-07-07 13:53:45 -0700

    Add animated dragon to title and game over screens

--- js/stateOver.js
+++ js/stateOver.js
@@ -2,11 +2,20 @@ var StateOver = {
   preload: function () {
 
     game.load.spritesheet("buttons", "assets/images/ui/buttons.png", 265, 75);
+    game.load.spritesheet('dragon', 'assets/images/main/dragon.png', 120, 85, 4);
   },
 
   create: function () {
     this.buttonPlayAgain = game.add.button(game.world.centerX, game.world.centerY + 100, 'buttons', this.replay, this, 0, 1, 0);
     this.buttonPlayAgain.anchor.set(0.5, 0.5);
+
+    this.dragon = game.add.sprite(game.world.centerX, game.world.centerY, 'dragon');
+    this.dragon.anchor.set(0.5, 0.5);
+    this.dragon.animations.add('fly', [0,1,2,3], 12, true);
+    this.dragon.animations.play('fly');
+    this.dragon.scale.x = -1;
+    game.stage.backgroundColor = '#26C9FF';
+
   },
 
   replay: function () {
--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,10 +1,18 @@
 var StateTitle = {
   preload: function () {
     game.load.spritesheet("buttons", "assets/images/ui/buttons.png", 265, 75);
+    game.load.spritesheet('dragon', 'assets/images/main/dragon.png', 120, 85, 4);
   },
   create: function () {
     this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, 'buttons', this.startGame, this, 6, 7, 6);
     this.buttonStart.anchor.set(0.5, 0.5);
+
+    this.dragon = game.add.sprite(game.world.centerX, game.world.centerY, 'dragon');
+    this.dragon.anchor.set(0.5, 0.5);
+    this.dragon.animations.add('fly', [0,1,2,3], 12, true);
+    this.dragon.animations.play('fly');
+    game.stage.backgroundColor = '#26C9FF';
+
   },
 
   startGame: function () {
