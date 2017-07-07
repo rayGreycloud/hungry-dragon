a434d3f23627625c23a206f11745d91ea0b903c6
rayGreycloud <rayGreycloud@gmail.com>
2017-07-07 13:47:16 -0700

    Add title screen

--- js/stateTitle.js
+++ js/stateTitle.js
@@ -1,5 +1,15 @@
 var StateTitle = {
-  preload: function () {},
-  create: function () {},
+  preload: function () {
+    game.load.spritesheet("buttons", "assets/images/ui/buttons.png", 265, 75);
+  },
+  create: function () {
+    this.buttonStart = game.add.button(game.world.centerX, game.world.centerY + 100, 'buttons', this.startGame, this, 6, 7, 6);
+    this.buttonStart.anchor.set(0.5, 0.5);
+  },
+
+  startGame: function () {
+    game.state.start('StateMain');
+  },
+
   update: function () {}
 }
